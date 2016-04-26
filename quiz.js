8c60719e79fcb584adcbe477c3ac6ef3655ac5ab
Adriana Recendez <Adriana Recendez>
2016-04-26 11:32:45 -0700

    quiz js file

---
+++ quiz.js
@@ -0,0 +1,16 @@
+var simple = 0, inter = 0, adv = 0;
+
+var userName = prompt("Hello, what's your name?");
+alert("Hello " + userName + ", let's get started!");
+
+while (simple < 20 && inter < 20 && adv < 20){
+	
+}
+
+if (simple == 20){
+	//code for ramen
+} else if (inter == 20){
+	//code for soup
+} else (adv == 20){
+	//code for take-out
+}
